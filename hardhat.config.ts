e454029eb2fdd4aeeafef746248fb153deba5980
Guilherme Correa <guilhermescorrea@hotmail.com.br>
2022-12-08 11:34:18 -0300

    Init Project

---
+++ hardhat.config.ts
@@ -0,0 +1,24 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://polygon-rpc.com",
+        blockNumber: 33603515,
+      }
+    }
+  }
+};
+
+export default config;
